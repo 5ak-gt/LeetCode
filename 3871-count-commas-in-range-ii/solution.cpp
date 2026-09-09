ac55407223af9f724d03b423ca4fa0804b72e54b
Saksham Sharma <sakshamgaming141@gmail.com>
2026-09-10 01:26:56 +0530

    Count Commas in Range II - 2026-09-09T19:56:55.020Z

---
+++ 3871-count-commas-in-range-ii/solution.cpp
@@ -0,0 +1,15 @@
+// 0 ms | 8.9 MB
+class Solution {
+public:
+    long long countCommas(long long n) {
+
+        long long ans = 0;
+
+        for (long long x = 1000; x<=n; x*=1000){
+            ans += n-x +1;
+
+        }
+
+        return ans;
+    }
+};
