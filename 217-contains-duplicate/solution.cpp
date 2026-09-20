ecec0697bf4bd265510f283f9e6d4f940ac94720
Saksham Sharma <sakshamgaming141@gmail.com>
2026-09-20 23:44:17 +0530

    Contains Duplicate - 2026-09-20T18:14:16.826Z

---
+++ 217-contains-duplicate/solution.cpp
@@ -0,0 +1,16 @@
+// 150 ms | 113.4 MB
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+
+        for(int x : nums) {
+            if(s.count(x))
+                return true;
+
+            s.insert(x);
+        }
+
+        return false;
+    }
+};
